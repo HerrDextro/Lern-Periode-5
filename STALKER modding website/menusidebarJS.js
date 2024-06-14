a48a24974736e4ea678c07554445e685fd465626
Neoow <142605719+HerrDextro@users.noreply.github.com>
2024-06-14 16:49:16 +0200

    Add files via upload
    neuere Version der Website

--- STALKER modding website/menusidebarJS.js
+++ STALKER modding website/menusidebarJS.js
@@ -40,3 +40,54 @@
     window.location.href = url;
 }
 
+//search bar function
+function performSearch() {
+  const query = document.getElementById('search-bar').value.toLowerCase();
+  const guides = document.querySelectorAll('.guide');
+
+  guides.forEach(guide => {
+      const title = guide.querySelector('h3').textContent.toLowerCase();
+      const description = guide.querySelector('.overlay p').textContent.toLowerCase();
+      
+      if (title.includes(query) || description.includes(query)) {
+          guide.style.display = 'block';
+      } else {
+          guide.style.display = 'none';
+      }
+  });
+}
+
+//login and signup windows js script
+//ignore for now
+// Get the modal
+var modal = document.querySelector('.login-modal');
+
+// Get the close button
+var closeButton = document.querySelector('.close');
+
+// Get the login link
+var signupLink = document.querySelector('.signup-link');
+
+// Function to open the modal
+function openModal() {
+    modal.style.display = 'block';
+}
+
+// Function to close the modal
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+// Close the modal when clicking on the close button
+closeButton.addEventListener('click', closeModal);
+
+// Open the modal when clicking on the signup link
+signupLink.addEventListener('click', openModal);
+
+// Close the modal when clicking anywhere outside of it
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+});
+
